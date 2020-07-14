1e152793746420b5f62b92065ee01df11b2fa60b
Epospiky <epospiky@gmail.com>
2020-07-14 12:03:46 +0100

    added index.php file

--- index.php
+++ index.php
@@ -4,8 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lighthouse</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/main.js"></script>
 </head>
 <body>
-    
+    <section class="header">
+        <nav class="navbar navbar-expand-lg nav-bg">
+            <a class="navbar-brand" href="#"><img src="assets/images/logo.png" alt="" class="logo-png"><span class="logo-text">Lighthouse</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarText">
+                <ul class="navbar-nav mr-auto nav-middle">
+                    <li class="nav-item item-pad active">
+                        <a class="nav-link-black" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item item-pad">
+                        <a class="nav-link-black" href="#">Features</a>
+                    </li>
+                    <li class="nav-item item-pad">
+                        <a class="nav-link-black" href="#">Download</a>
+                    </li>
+                    <li class="nav-item item-pad">
+                        <a class="nav-link-black" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2 search-form" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0 search-btn" type="submit"><img src="assets/images/search.png" alt=""></button>
+                </form>
+            </div>
+        </nav>  
+        <header class="header-cont">
+            <div class="row">
+                <div class="intro col-md-6">
+                    <h1>Meet <br><span class="lh">LightHouse</span></h1>
+                    <p>The smart way to keep tabs on your kids location <br> 
+                        and  interactions, making sure they are safe at all <br> 
+                        times.</p>
+                    <a href="#" class="watch-btn">Watch video &nbsp;<span class="download-icon"><img src="assets/images/play.png" alt=""></span></a>
+                    <ul class="bully-icons">
+                        <li class="item"><img src="assets/images/bully1.png" alt=""></li>
+                        <li class="item"><img src="assets/images/bully2.png" alt=""></li>
+                        <li class="item"><img src="assets/images/bully3.png" alt=""></li>
+                        <li class="item"><img src="assets/images/bully4.png" alt=""></li>
+                    </ul>
+                </div>
+                <div class="svg-img col-md-6">
+                    <img src="assets/images/kids.png" alt="">
+                </div>
+            </div>
+        </header>      
+    </section>
 </body>
 </html>
